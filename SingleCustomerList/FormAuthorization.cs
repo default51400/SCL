ea621bc6624eb4ce408239e9232945c95ee9d069
Oleksii <abcentqip@mail.ru>
2018-07-11 18:08:21 +0300

    attempts to reach the token

--- SingleCustomerList/FormAuthorization.cs
+++ SingleCustomerList/FormAuthorization.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Management;
 
 namespace SingleCustomerList
 {
@@ -16,7 +17,7 @@ namespace SingleCustomerList
         public FormAuthorization()
         {
             InitializeComponent();
-            textBoxLanguage.Text = InputLanguage.CurrentInputLanguage.Culture.ThreeLetterWindowsLanguageName;
+            textBoxLanguage.Text = InputLanguage.CurrentInputLanguage.Culture.TwoLetterISOLanguageName.ToUpper();
             InitializeACSK();
             InitializeTokens();
         }
@@ -28,17 +29,37 @@ namespace SingleCustomerList
             comboBoxACSK.Items.Add("АЦСК Приватбанк");
             comboBoxACSK.SelectedIndex = 0;
         }
-
+ 
         private void InitializeTokens()
         {
-            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            //foreach (DriveInfo drive in DriveInfo.GetDrives())
+            //{
+            //    //if (drive.DriveType == DriveType.Removable)
+            //    //{
+            //        comboBoxSelectToken.Items.Add(drive.Name);
+            //    //}
+            //}
+            string str = "1";
+            
+            foreach (ManagementObject device in new ManagementObjectSearcher(@"SELECT * FROM Win32_PnpEntity").Get())
             {
-                if (drive.DriveType == DriveType.Removable)
-                {
-                    comboBoxSelectToken.Items.Add(drive.Name);
-                }
+                //var example = device.GetPropertyValue.Contains();
+                string deviceID = (string)device.GetPropertyValue("Caption");
+                str += deviceID + "\n";
+
+            }
+
+            MessageBox.Show(str,"месседжбокс");
+
+
+            var drives = DriveInfo.GetDrives().Where(driv => driv.IsReady);
+
+            foreach (DriveInfo drive in drives)
+            {
+                comboBoxSelectToken.Items.Add(drive.Name);
             }
             comboBoxSelectToken.SelectedIndex = 0;
+            //MessageBox.Show("asd", System.Windows.);
         }
         private void buttonExit_Click(object sender, EventArgs e)
         {
